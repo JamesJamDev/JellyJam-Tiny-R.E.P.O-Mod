5fc0022e0d299050065156967132b1c59da1d996
JamesJDev <jellyjambusiness@gmail.com>
2025-03-15 15:21:54 -0400

    Initial Project Template Added

---
+++ JellyJamMadness/JellyJamTinyREPO/JellyJamTinyREPO/ExamplePlayerControllerPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace JellyJamTinyREPO
+{
+    [HarmonyPatch(typeof(PlayerController))]
+    internal static class ExamplePlayerControllerPatch
+    {
+        [HarmonyPrefix, HarmonyPatch("Update")]
+        private static void Update_Prefix(PlayerController __instance)
+        {
+            // Code to execute for each PlayerController *before* Update() is called.
+            JellyJamTinyREPO.Logger.LogDebug($"{__instance} Update Prefix");
+        }
+
+        [HarmonyPostfix, HarmonyPatch("Update")]
+        private static void Update_Postfix(PlayerController __instance)
+        {
+            // Code to execute for each PlayerController *after* Update() is called.
+            JellyJamTinyREPO.Logger.LogDebug($"{__instance} Update Postfix");
+        }
+    }
+}
